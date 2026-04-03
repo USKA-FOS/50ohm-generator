3574d0addd1045a40322a956d7bd7706d2998438
Mathias Weyland <hb9frv@uska.ch>
2026-04-03 14:10:15 +0200

    Bugfix: Builder crashed on missing upstream questions
    When operating in diff mode (see #12), the builder would crash when a
    question was removed from upstream but still present in our own pool.
    This happend to EI303 and is fixed with this commit.

--- src/build.py
+++ src/build.py
@@ -61,20 +61,21 @@ class Build:
             metadata_json = json.load(file)
 
             question = None
+            question_upstream = None
             metadata = None
 
             if number in self.questions:
                 question = self.questions[number]
+                # FIXME: Remove after beta
+                if number in self.questions_upstream:
+                    question_upstream = self.questions_upstream[number]
+                else:
+                    question_upstream = question.copy()
+                    question_upstream['question'] = None  # This is how we encode new questions
+
 
             if number in metadata_json:
                 metadata = metadata_json[number]
-
-            # FIXME: Remove after beta
-            if number in self.questions_upstream:
-                question_upstream = self.questions_upstream.get(number, None)
-            else:
-                question_upstream = question.copy()
-                question_upstream['question'] = None  # This is how we encode new questions
 
             if question is None or metadata is None:
                 tqdm.write(
@@ -85,6 +86,9 @@ class Build:
                 )
                 metadata = {"layout": "not-found", "picture_a": ""}
                 question = {"question": f"Frage {input} nicht gefunden"}
+
+            if question_upstream is None:
+                question_upstream = {"question": f"Frage {input} nicht gefunden"}
 
             if "answer_a" in question:
                 answers = [question["answer_a"], question["answer_b"], question["answer_c"], question["answer_d"]]
