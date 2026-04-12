2ee16012bb727f61e4f5e2cb3ed369f79add35b7
Mathias Weyland <hb9frv@uska.ch>
2026-04-12 12:07:34 +0200

    Better handling of periods in word-diff
    Previously, word diff would replace a word by the same word followed
    by a period ('.') if a period was added during the quesiton pool review.
    With this change, trailing periods are detected and highlighted in green
    without removing the preceding word.

--- hb_beta.py
+++ hb_beta.py
@@ -18,6 +18,14 @@ def diff_filter(new: str, old: str):
         elif tag == 'insert':
             ret += f'<span class="diff added">{" ".join(new_words[j1:j2])}</span>'
         elif tag == 'replace':
-            ret += f'<span class="diff removed">{" ".join(old_words[i1:i2])}</span>'
-            ret += f'<span class="diff added">{" ".join(new_words[j1:j2])}</span>'
+            deleted = ' '.join(old_words[i1:i2])
+            inserted = ' '.join(new_words[j1:j2])
+            # Shortcut to deal with added or removed puncutuation.
+            if deleted + '.' == inserted:
+                ret += deleted + '<span class="diff added">.</span>'
+            elif deleted == inserted + '.':
+                ret += inserted + '<span class="diff removed">.</span>'
+            else:
+                ret += f'<span class="diff removed">{deleted}</span>'
+                ret += f'<span class="diff added">{inserted}</span>'
     return ret
